4d09a32c1153246dcbca1abf3ccecb17e0ba831c
s18547a <s18547@pjwstk.edu.pl>
2023-06-27 16:06:32 +0200

    sc

--- src/apiCalls/config/domain.ts
+++ src/apiCalls/config/domain.ts
@@ -1,2 +1,2 @@
-export const domain = 'localhost:8000';
-//export const domain = 'vetapprestapi.azurewebsites.net';
+//export const domain  = 'localhost:8000';
+export const domain = 'vetapprestapi.azurewebsites.net';
